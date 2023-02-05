ae70f57b8f48ba3c84e9507b1046216d0f8b314e
ArpitGupta1996 <68arpit@gmail.com>
2023-02-05 16:31:37 +0530

    dashbaord admin page

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::get('/', function () {
 });
 
 Route::post('contact', [ContactController::class,'contact'])->name('contact');
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'),'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
